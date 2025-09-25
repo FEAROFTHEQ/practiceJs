15202d9e3faf8a695b85f86dafeda68453008b5c
Alina Y. <devadevam.tod@gmail.com>
2025-09-25 16:19:45 +0300

    first task done

--- src/js/task-1.js
+++ src/js/task-1.js
@@ -4,14 +4,9 @@ const li = document.querySelectorAll(".item");
 console.log("Number of categories: ", categories.children.length);
 
 li.forEach((child) => {
-  const itemChildren = child.children;
-  const [h2, ul] = itemChildren;
-  // if (itemChildren.tagName === "h2") {
-  console.log("Category: ", h2.textContent);
-  // }
-  // if (itemChildren.tagName === "ul") {
-  console.log("Elements: ", ul.children.length);
-  // }
+  const title = child.querySelector("h2");
+  console.log("Category: ", title.textContent);
+  const elements = child.querySelectorAll("li");
+  console.log("Elements: ", elements.length);
   console.log("meow");
-  //   console.log(itemChildren);
 });
